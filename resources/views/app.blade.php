0d4cfb90d98419456398a09709b65f7ea618b8c2
Adnan Hamzeh <adnan.37h@gmail.com>
2019-10-15 00:33:42 +0300

    Reorder scripts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,6 @@
 
     <body>
         <div id="app"></div>
-        <script src="{{asset('js/app.js')}}"></script>
         <script>
             window.laravel = {
                 CLIENT_ID: '{{env("CLIENT_ID")}}',
@@ -22,6 +21,7 @@
                 HOST: '{{env("APP_URL")}}'
             }
         </script>
+        <script src="{{asset('js/app.js')}}"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
     </body>
 
